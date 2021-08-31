321daa9c9ac1d4ebea0da01c8561ab9d0637f3a4
Ankan Mandal <72788172+ankan-mandal-1@users.noreply.github.com>
2021-08-31 13:26:34 +0530

    Add files via upload

---
+++ public/savebtn.js
@@ -0,0 +1,5 @@
+function changeBtnColor(){
+    document.querySelector('.save').style.background = "#e16259";
+    document.querySelector('.save').style.borderColor = "#be5643";
+    document.querySelector('.save').style.cursor = "pointer";
+}
